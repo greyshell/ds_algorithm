d10de6607270fc739fffd00e1a6ec587d3a3d32c
Abhijit Sinha <grey.shell@gmail.com>
2020-11-26 16:24:16 -0500

    [skip travis] update sort

--- heap/README.md
+++ heap/README.md
@@ -1,35 +1,40 @@
 # Problems on Heap
 
 
-##### Merge k Sorted linked list
+##### merge k sorted linked list
 
 ```
-Solution 1:
+solution:
 - create a min heap of size k.
 - pick the first element from each linked list and push into the heap.
-- when the heap is full, pop the min item.
+- when the heap is full, pop the item.
 - pick the next item from the same array and push it into the heap
+
+solution_02:
+- if all linkedlist elements are integer then combine all those lists and use counting sort -> O(n)
+- for non-integer elements: quick sort -> O(n*log(n))
 ```
 
 
-##### Find k largest elements in an array
+##### find k largest elements in an array
 
 ```
-Solution 1:
+solution:
+- create a min heap of size k
+- insert each array elements into the the heap
+- after inserting an element, check if the heap overflows then pop an item to maintain the size
+- time complexity: O(n*log(k)), works faster when k > n/2
+- space complexity: O(k)
+
+solution_02:
 - build the max heap from that array -> O(n)
-- pop out the max element k times
-- time complexity:  O(n + k*log(n))
+- pop the elements k times
+- time complexity:  O(n + k*log(n)), works faster when k <= n/2
 - space complexity: O(1)
 
-Solution 2:
-- use non comparison based sorting like counting sort if the elements are integer
+solution_03:
+- if all array elements are integer then use non comparison based sorting like counting sort 
 - time complexity: O(n)
 - space complexity: O(n) -> for auxiliary array
 
-Solution 3:
-- create a min heap of size k
-- iterate the array element and insert into the heap
-- after push check if the heap overflows, then pop the item
-- time complexity: O(n*log(k)), O(log(k) -> 1
-- space complexity: O(k)
 ```
--- heap/kth_largest_elements_array.c
+++ heap/kth_largest_elements_array.c
@@ -1,15 +1,18 @@
 /*
  * author: greyshell
- * description: find kth max elements in an array
+ * description: find kth max elements in an array without sorting
+ *  - best comparison based sorting would take O(n*log(n)) time
+ *  - if the items are integers, then non-comparison based sorting (counting sort) would take O(n)
+ *  time
  * */
 
 #include <stdio.h>
 #include "stdbool.h"
-#include "../liquid_oxygen/heap/heap.h"
+#include "../libO2/include/binary_heap_dynamic_array_int.h"
 
 void k_largest_elements_02(int *arr, int n, int k) {
     /*
-     * time complexity: O(n + k*log(n))
+     * time complexity: O(n + k*log(n)), works faster when k <= n/2
      * space complexity: O(1)
      */
     heap h;
@@ -29,21 +32,23 @@ void k_largest_elements_02(int *arr, int n, int k) {
 
 void k_largest_elements(int *arr, int n, int k) {
     /*
-     * time complexity: O(n*log(k)), O(log(k) -> 1 hence it will be O(n)
+     * time complexity: O(n*log(k)), works faster when k > n/2
      * space complexity: O(k)
+     * works better when k > n/2
      */
     heap h;
     size_t heap_size;
     int data;
 
-    // create a min heap of size k
+    // create a min heap, initial capacity = k
+    // heap implementation is backed by dynamic array so we can insert any nos of elements
     initialize_heap(&h, k, false);
 
     for (int i = 0; i < n; i++) {
         push_heap(&h, arr[i]);
         get_heap_size(&h, &heap_size);
-        // check if the heap overflows
-        if (heap_size == k + 1) {
+        // if the heap size exceeds k, pop an item to maintain the size = k
+        if (heap_size > k) {
             pop_heap(&h, &data);
         }
     }
@@ -54,8 +59,8 @@ void k_largest_elements(int *arr, int n, int k) {
 }
 
 int main(void) {
-    int arr[] = {10, 2, 30, 50, 1, 70};
-    int n = 6, k = 3;
+    int arr[] = {10, 2, 30, 50, 200, 70};
+    int n = 6, k = 4;
     k_largest_elements(arr, n, k);
     k_largest_elements_02(arr, n, k);
     return 0;
