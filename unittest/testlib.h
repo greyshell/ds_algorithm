ce987833c6cc7b893d1bbb1e2e86b448fc059c39
Abhijit Sinha <grey.shell@gmail.com>
2020-11-29 03:11:39 -0500

    quick sort

--- unittest/testlib.h
+++ unittest/testlib.h
@@ -5,6 +5,10 @@
 #ifndef UNIT_TEST_H__
 #define UNIT_TEST_H__
 
+// heap/kth_largest_elements_immutable_max_heap.c
+void kth_largest_elements_immutable_max_heap(int *test_arr, int n, int k, int *out_arr);
+
+
 // heap/kth_largest_elements_array.c
 void kth_largest_elements_array(int *, int, int, int *);
 void kth_largest_elements_array_02(int *, int, int, int *);
