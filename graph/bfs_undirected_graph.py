ec98b15805cddb36040cf862ddf46c829f51a97a
ABHIJIT SINHA <grey.shell@gmail.com>
2021-06-12 00:50:03 -0400

    refined graph code
    Signed-off-by: ABHIJIT SINHA <grey.shell@gmail.com>

--- graph/bfs_undirected_graph.py
+++ graph/bfs_undirected_graph.py
@@ -7,35 +7,35 @@ from collections import deque
 from graph_adt import Vertex, UndirectedGraph
 
 
-def bfs(graph, src_v):
+def bfs(graph, src_vertex_name):
     """
     Breadth first search
     time complexity: O(V + E)
     space complexity: O(V) -> to maintain the visited set
     """
-    if not graph.get_vertex(src_v):
+    if not graph.get_vertex_obj(src_vertex_name):
         return False
 
     # track the visited vertices
     visited = set()
     # add the source vertex into the visited set
-    visited.add(src_v)
+    visited.add(src_vertex_name)
 
     # add the source vertex into the queue
     queue = deque()
-    queue.append(src_v)
+    queue.append(src_vertex_name)
 
     while queue:
         # pop the vertex from the queue
-        vertex = queue.popleft()
-        print(vertex, end=" ")
+        vertex_name = queue.popleft()
+        print(vertex_name, end=" ")
         # iterate all neighbors of that node
-        vertex_node = graph.get_vertex(vertex)
-        for neighbor in vertex_node.get_neighbors():
+        vertex_obj = graph.get_vertex_obj(vertex_name)
+        for neighbor_obj in vertex_obj.get_all_neighbors_obj():
             # if that neighbor node is not visited then add to the queue
-            if neighbor.get_key() not in visited:
-                visited.add(neighbor.get_key())
-                queue.append(neighbor.get_key())
+            if neighbor_obj.get_vertex_name() not in visited:
+                visited.add(neighbor_obj.get_vertex_name())
+                queue.append(neighbor_obj.get_vertex_name())
 
 
 def main():
@@ -55,11 +55,11 @@ def main():
     undirected_graph.add_edge(3, 4)
     undirected_graph.add_edge(3, 5)
 
-    src_v = 0
+    src_vertex_name = 0
 
     print("")
     print(f"bfs traversal: ")
-    bfs(undirected_graph, src_v)
+    bfs(undirected_graph, src_vertex_name)
 
 
 if __name__ == '__main__':
