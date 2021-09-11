5a5cbae93ce59d79f29122dd842c514ff287749f
ABHIJIT SINHA <grey.shell@gmail.com>
2021-09-11 08:47:59 -0400

    added 3sum
    Signed-off-by: ABHIJIT SINHA <grey.shell@gmail.com>

--- hashing/three_sum.py
+++ hashing/three_sum.py
@@ -7,13 +7,14 @@ def three_sum(arr: list, target: int) -> bool:
     time: O(n^2)
     space: O(n)
     """
-    temp_arr = list()
+    lookup = set()
     for i in range(0, len(arr)):
         for j in range(i + 1, len(arr)):
-            temp_arr.append(arr[i] + arr[j])
+            lookup.add(arr[i] + arr[j])
 
     for i in range(0, len(arr)):
-        if temp_arr[i] - target == 0:
+        num = arr[i]
+        if (target - num) in lookup:
             return True
 
     return False
